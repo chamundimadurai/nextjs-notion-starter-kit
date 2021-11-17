608c60cc9063ae31ed523b416a133e59e7b3e81a
chamundimadurai <94557531+chamundimadurai@users.noreply.github.com>
2021-11-17 23:06:05 +0530

    Updated to Personal Data

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'b7ffbd15991f4616b4b03aa876de3965',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chamundi Pakku Madurai',
+  domain: 'chamundipakkumadurai.ml',
+  author: 'Chamundi Madurai',
 
-  // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+//   // open graph metadata (optional)
+//   description: 'Example site description',
+//   socialImageTitle: 'Transitive Bullshit',
+//   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+//   twitter: 'transitive_bs',
+//   github: 'transitive-bullshit',
+//   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+//   // page-specific values will override these site-wide defaults
+//   defaultPageIcon: null,
+//   defaultPageCover: null,
+//   defaultPageCoverPosition: 0.5,
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
